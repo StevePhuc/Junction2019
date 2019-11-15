a74c9865a9c51d9ad16d9cc569c00c2533c1288f
Steve Phuc <stevephuc.dev@gmail.com>
2019-11-16 00:23:59 +0200

    setup up home page

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './junction.png';
+import businessFinland from './businessFinland.png';
 import './App.css';
+import Bottom from './components/Bottom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="main">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.hackjunction.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Junction 2019
         </a>
+        <img src={businessFinland} className="businessFinland-logo" alt="logo" />
+        <p>
+          The <code>DNA</code> of Helsinki.
+        </p>
       </header>
+      <Bottom />
     </div>
   );
 }
